7c550094819474b872e7dc52fd2265ba16efa40e
Artem Zholud <zholud.artem@gmail.com>
2022-05-04 14:52:57 +0200

    правки по линтеру

--- source/js/modules/feedback.js
+++ source/js/modules/feedback.js
@@ -4,7 +4,6 @@ const feedbackSwiper = feedback.querySelector('.swiper');
 const feedbackSwiperButtonPrev = feedbackSwiper.querySelector('.feedback__swiper-button-prev');
 const feedbackSwiperButtonNext = feedbackSwiper.querySelector('.feedback__swiper-button-next');
 
-
 export const swiper = new Swiper(feedbackSwiper, {
   slidesPerView: 1,
   spaceBetween: 56,
--- source/js/modules/trainers.js
+++ source/js/modules/trainers.js
@@ -34,4 +34,3 @@ export const swiper = new Swiper(trainersSwiper, {
     },
   },
 });
-
